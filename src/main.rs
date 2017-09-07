5b0290ede946922147f5c5b1539cf7ef26ec4af8
Adrian Mouat <adrian.mouat@gmail.com>
2017-09-07 17:26:17 +0100

    Reformated.

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,23 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use std::path::Path;
-use rocket_contrib::{Json};
-use rocket::http::{Status};
-use rocket::response::{Response, Responder, NamedFile};
+use rocket_contrib::Json;
+use rocket::http::Status;
+use rocket::response::{NamedFile, Responder, Response};
 use rocket::request::Request;
 use rocket::response::status::NotFound;
 
-#[derive(Serialize)]
-#[derive(Debug)]
-struct V2AvailableRoutes {
-}
+#[derive(Serialize, Debug)]
+struct V2AvailableRoutes {}
 
 #[derive(Debug)]
 struct RegistryResponse<R>(pub R);
 
-impl <'r, R: Responder<'r>> Responder<'r> for RegistryResponse<R> {
+impl<'r, R: Responder<'r>> Responder<'r> for RegistryResponse<R> {
     fn respond_to(self, req: &Request) -> Result<Response<'r>, Status> {
         let header = rocket::http::Header::new("Docker-Distribution-API-Version", "registry/2.0");
         Response::build()
@@ -31,34 +30,37 @@ impl <'r, R: Responder<'r>> Responder<'r> for RegistryResponse<R> {
 }
 
 type LycaonResponse<A> = RegistryResponse<Json<A>>;
-type MaybeResponse<A, E>  = Result<Json<A>, NotFound<Json<E>>>;
+type MaybeResponse<A, E> = Result<Json<A>, NotFound<Json<E>>>;
 
 
 #[get("/v2")]
 fn get_v2root() -> LycaonResponse<V2AvailableRoutes> {
-    RegistryResponse::<Json<V2AvailableRoutes>> ((Json(V2AvailableRoutes {})))
+    RegistryResponse::<Json<V2AvailableRoutes>>((Json(V2AvailableRoutes {})))
 }
 
 #[get("/v2/<name>/manifests/<reference>")]
-fn get_manifest(name: String, reference: String) ->
-    MaybeResponse<V2AvailableRoutes, V2AvailableRoutes> {
-        println!("Getting Manifest");
-        match reference.as_str() {
-            "good" => Ok(Json(V2AvailableRoutes {})),
-            _      => Err(NotFound(Json(V2AvailableRoutes {}))),
-        }
+fn get_manifest(
+    name: String,
+    reference: String,
+) -> MaybeResponse<V2AvailableRoutes, V2AvailableRoutes> {
+    println!("Getting Manifest");
+    match reference.as_str() {
+        "good" => Ok(Json(V2AvailableRoutes {})),
+        _ => Err(NotFound(Json(V2AvailableRoutes {}))),
+    }
 }
 
 #[get("/v2/<name>/blobs/<digest>")]
-fn get_blob(name: String, digest: String) ->
-    MaybeResponse<V2AvailableRoutes, V2AvailableRoutes> {
-        println!("Getting Blob");
-        match digest.as_str() {
-            "good" => Ok(Json(V2AvailableRoutes {})),
-            _      => Err(NotFound(Json(V2AvailableRoutes {}))),
-        }
+fn get_blob(name: String, digest: String) -> MaybeResponse<V2AvailableRoutes, V2AvailableRoutes> {
+    println!("Getting Blob");
+    match digest.as_str() {
+        "good" => Ok(Json(V2AvailableRoutes {})),
+        _ => Err(NotFound(Json(V2AvailableRoutes {}))),
+    }
 }
 
-fn main () {
-    rocket::ignite().mount("/", routes![get_v2root, get_manifest, get_blob]).launch();
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![get_v2root, get_manifest, get_blob])
+        .launch();
 }
