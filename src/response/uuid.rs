654edd00b71cd88dfea17794d20603d9915d5539
Hamish Hutchings <moredhel@aoeu.me>
2017-09-27 12:22:36 +0100

    Refactor and add macro for Responder Trait

--- src/response/uuid.rs
+++ src/response/uuid.rs
@@ -18,18 +18,24 @@ pub enum UuidResponse {
     },
     Empty,
 }
+DummyResponder!(UuidResponse);
 
 impl RegistryTrait for UuidResponse {
-    fn ok<'r>(self, _req: &Request) -> Result<Response<'r>, Status> {
+    fn ok<'r>(&self, _req: &Request) -> Result<Response<'r>, Status> {
         debug!("Uuid Ok");
 
-        if let UuidResponse::Uuid {uuid, name, repo, left, right} = self {
-            let location_url = format!("{}/v2/{}/{}/blobs/uploads/{}?query=true", BASE_URL, name, repo, uuid);
-            let upload_uuid = Header::new("Docker-Upload-UUID", uuid);
+        if let &UuidResponse::Uuid {ref uuid, ref name, ref repo, ref left, ref right} = self {
+            let location_url = format!("{}/v2/{}/{}/blobs/uploads/{}?q=t",
+                                       BASE_URL,
+                                       name,
+                                       repo,
+                                       uuid);
+            let upload_uuid = Header::new("Docker-Upload-UUID", uuid.clone());
             let range = Header::new("Range", format!("{}-{}", left, right));
-            debug!("Range: {}-{}, Length: {}", left, right, right-left);
             let length = Header::new("Content-Length", format!("{}", right - left));
             let location = Header::new("Location", location_url);
+
+            debug!("Range: {}-{}, Length: {}", left, right, right-left);
             Response::build()
                 .header(upload_uuid)
                 .header(location)
@@ -41,17 +47,11 @@ impl RegistryTrait for UuidResponse {
         } else { panic!("oh noes!") }
     }
 
-    fn err<'r>(self, _req: &Request) -> Result<Response<'r>, Status> {
+    fn err<'r>(&self, _req: &Request) -> Result<Response<'r>, Status> {
         debug!("Uuid Error");
         Response::build()
             .status(Status::NotFound)
             .ok()
-    }
-}
-
-impl<'r> Responder<'r> for UuidResponse {
-    fn respond_to(self, _: &Request) -> Result<Response<'r>, Status> {
-        panic!("please use the RegistryTrait::{ok,err} functions")
     }
 }
 
