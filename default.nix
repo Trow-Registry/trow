c6ffe98f4e93e55274ffcb79ca99d5426a4b9691
Hamish Hutchings <moredhel@aoeu.me>
2017-11-21 10:00:44 +0000

    Add specific version to nix-shell

--- default.nix
+++ default.nix
@@ -1,14 +1,23 @@
 with import <nixpkgs> {};
+let
+  date = "2017-11-15";
+  rust-nightly = mozilla.rustChannels.rustChannelOf { date = date; channel = "nightly"; };
+in
 stdenv.mkDerivation rec {
   name = "lycaon";
   env = buildEnv { name = name; paths = buildInputs; };
+  # src = ./.;
   buildInputs = [
     pkgconfig
-    latest.rustChannels.nightly.rust
+    rust-nightly.rust
     capnproto
 
     # dev
     rustfmt
     valgrind
   ];
+
+  shellHook = ''
+    echo Rust Nightly: ${date}
+  '';
 }
