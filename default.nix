60ed8f3524386f9065a25fdbcbaed8fe8c90f55f
Hamish Hutchings <moredhel@aoeu.me>
2017-12-08 14:14:40 +0300

    Add updated default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,22 @@
 with import <nixpkgs> {};
 let
+  pkgs = import <nixpkgs> {};
   date = "2017-11-15";
-  rust-nightly = mozilla.rustChannels.rustChannelOf { date = date; channel = "nightly"; };
+  mozilla-overlay = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "661f3f4d8183f493252faaa4e7bf192abcf5d927";
+    sha256 = "0g1ig96a5qzppbf75qwll8jvc7af596ribhymzs4pbws5zh7mp6p";
+  };
+  mozilla = (import mozilla-overlay) pkgs pkgs;
+  rust-nightly = mozilla.rustChannelOf { date = date; channel = "nightly"; };
 in
-# rustPlatform.buildRustPackage rec {
-stdenv.mkDerivation rec {
+rustPlatform.buildRustPackage rec {
+# stdenv.mkDerivation rec {
   name = "lycaon";
   env = buildEnv { name = name; paths = buildInputs; };
-  # src = ./.;
-  # cargoSha256 = "0q68qyl2h6i0qsz82z840myxlnjay8p1w5z7hfyr8fqp7wgwa9cx";
+  src = lib.sourceFilesBySuffices ./. [".lock" ".toml" ".rs"];
+  cargoSha256 = "0q68qyl2h6i0qsz82z840myxlnjay8p1w5z7hfyr8fqp7wgwa9cx";
   buildInputs = [
     pkgconfig
     rust-nightly.rust
@@ -28,6 +36,7 @@ stdenv.mkDerivation rec {
   configurePhase = ''true'';
 
   buildPhase = ''
+    echo "Starting the build"
     cargo build -vv
   '';
 
