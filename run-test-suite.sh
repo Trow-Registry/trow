ee99f47f715d46dd501c292f2de391c57fa31391
Hamish Hutchings <moredhel@aoeu.me>
2017-12-22 13:20:53 +0300

    Whitespace cleanup

--- run-test-suite.sh
+++ run-test-suite.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # we could add a random string on the end of vars to avoid collisions
-container="lyc-reg-test" 
+container="lyc-reg-test"
 network="lyc-reg-test-net"
 
 docker network create ${network}
