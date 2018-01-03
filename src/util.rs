26f472b195b1a6c92f18310c32034ac6f40680b1
Hamish Hutchings <moredhel@aoeu.me>
2018-01-03 12:10:42 +0300

    Clean up unused utility functions

--- src/util.rs
+++ src/util.rs
@@ -4,25 +4,7 @@
 
 use failure::Error;
 
-use config;
 use errors;
-
-// --- Channel Helpers ---
-pub fn send(tx: config::SendSock, msg: config::BackendMessage) -> Result<(), Error> {
-    tx.send(msg).or_else(|e| {
-        warn!("Error: {}", e);
-        Err(e.into())
-    })
-}
-
-pub fn recv(rx: &config::RecvSock) -> Result<config::BackendMessage, Error> {
-    use std::time;
-    let duration = time::Duration::from_secs(10);
-    rx.recv_timeout(duration).or_else(|e| {
-        warn!("{}", e);
-        Err(e.into())
-    })
-}
 
 pub fn std_err(msg: &str) -> Error {
     Error::from(errors::Server::GenericError(msg.to_owned()))
