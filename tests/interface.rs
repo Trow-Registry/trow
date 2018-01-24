18efecf84a02caf00a0b590cc41abdec533b646c
Adrian Mouat <adrian.mouat@gmail.com>
2018-01-24 15:06:05 +0000

    Finally got a simple test working and moved to Rocket master.

--- tests/interface.rs
+++ tests/interface.rs
@@ -1,7 +1,7 @@
-/// Test API end-points
-extern crate environment;
+extern crate tokio_core;
+extern crate hyper;
 extern crate futures;
-extern crate curl;
+extern crate environment;
 
 #[cfg(test)]
 mod interface_tests {
@@ -12,16 +12,18 @@ mod interface_tests {
     use std::process::Child;
     use std::time::Duration;
     use std::thread;
-    use curl::easy::Easy;
+    use hyper::{Client, StatusCode, Error, Response};
+    use tokio_core::reactor::Core;
 
     struct LycaonInstance {
-        pid: Child,
+        pid: Child
     }
     /// Call out to cargo to start lycaon.
     /// Seriously considering moving to docker run.
 
     fn start_lycaon() -> LycaonInstance {
         let child = Command::new("cargo")
+            //.current_dir("../../")
             .arg("run")
             .env_clear()
             .envs(Environment::inherit().compile())
@@ -30,7 +32,7 @@ mod interface_tests {
 
         //FIXME: change to poll for start-up
         thread::sleep(Duration::from_millis(500));
-        LycaonInstance { pid: child }
+        LycaonInstance{pid: child}
     }
 
     impl Drop for LycaonInstance {
@@ -40,14 +42,24 @@ mod interface_tests {
         }
     }
 
+    fn get_sync(url: &str) -> Result<Response, Error> {
+
+        let mut core = Core::new().expect("Failed to start hyper");
+        let client = Client::new(&core.handle());
+
+        let uri = url.parse()?;
+        let work = client.get(uri);
+        core.run(work)
+    }
+
     #[test]
     fn get_main() {
         let _lyc = start_lycaon();
-        let mut easy = Easy::new();
-        easy.url("http://localhost:8000").unwrap();
-        easy.perform().unwrap();
 
-        assert_eq!(easy.response_code().unwrap(), 200);
+        let i = get_sync("http://localhost:8000").unwrap();
+        assert_eq!(i.status(), StatusCode::Ok);
+        assert_eq!(i.headers().get_raw("Docker-Distribution-API-Version").unwrap(), "registry/2.0");
+
     }
 
 }
