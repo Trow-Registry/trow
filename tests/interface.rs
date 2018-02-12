124a234c06faebb6ed43a17f5447d0302271c5d4
Hamish Hutchings <moredhel@aoeu.me>
2018-02-12 14:13:30 +0300

    Beginning of Manifest interface test.
    See:
    https://github.com/ContainerSolutions/manifest-sample-python/blob/master/construct-manifest.py
    for information on how to implement manifest generation

--- tests/interface.rs
+++ tests/interface.rs
@@ -3,6 +3,8 @@ extern crate environment;
 extern crate futures;
 #[macro_use]
 extern crate hyper;
+extern crate jwt;
+extern crate rustc_serialize;
 extern crate rand;
 extern crate tokio_core;
 
@@ -28,6 +30,7 @@ mod interface_tests {
     use futures::Stream;
 
     const LYCAON_ADDRESS: &'static str = "http://localhost:8000";
+    // const MANIFEST_TEMPLATE: &'static str = "./tests/manifest-template.json";
 
     header! { (DistributionApi, "Docker-Distribution-API-Version") => [String] }
     header! { (UploadUuid, "Docker-Upload-Uuid") => [String] }
@@ -64,7 +67,7 @@ mod interface_tests {
     fn setup() {
         // create dummy layer
         use std::fs;
-        fs::create_dir_all ("./data/layers/test/test").unwrap();
+        fs::create_dir_all("./data/layers/test/test").unwrap();
         fs::File::create("./data/layers/test/test/test_digest").unwrap();
 
     }
@@ -83,6 +86,129 @@ mod interface_tests {
             blob.push(rng.gen::<u8>());
         }
         blob
+    }
+
+    // fn generate_manifest(digest: &str) -> Result<Value, ()> {
+    //     use std::fs::File;
+    //     use std::io::Read;
+    //     let mut template = File::open(MANIFEST_TEMPLATE).unwrap();
+    //     let mut contents = String::new();
+    //     template.read_to_string(&mut contents).unwrap();
+    //     let mut v: Value = serde_json::from_str(&contents).unwrap();
+    //     v["fsLayers"][0]["blobSum"] = Value::String(digest.to_string());
+    //     Ok(v)
+    // }
+
+    #[derive(Clone, Debug, Default, RustcDecodable, RustcEncodable)]
+    struct EmptyStruct {}
+
+    #[derive(Clone, Debug, Default, RustcDecodable, RustcEncodable)]
+    #[allow(non_snake_case)]
+    struct BlobSummary {
+        blobSum: String
+    }
+
+    #[derive(Debug, RustcDecodable, RustcEncodable)]
+    #[allow(non_snake_case)]
+    struct SigningManifest {
+        schemaVersion: u8,
+        name: String,
+        tag: String,
+        architecture: String,
+        fsLayers: Vec<BlobSummary>,
+        history: Vec<EmptyStruct>
+    }
+
+    impl Default for SigningManifest {
+        fn default() -> Self {
+            SigningManifest {
+                schemaVersion: 1,
+                name: "test/test".to_owned(),
+                tag: "latest".to_owned(),
+                architecture: "amd64".to_owned(),
+                fsLayers: Default::default(),
+                history: Default::default(),
+            }
+        }
+    }
+
+    #[derive(Debug, Default)]
+    struct SignatureJWK {
+        crv: String,
+        kty: String,
+        x: String,
+        y: String,
+    }
+
+    #[derive(Debug, Default)]
+    struct SignatureHeader {
+        alg: String,
+        jwk: SignatureJWK
+    }
+
+    #[derive(Debug, Default)]
+    struct Signature {
+        header: SignatureHeader,
+        payload: String,
+        protected: String,
+        signature: String,
+    }
+
+    #[derive(Debug, Default)]
+    #[allow(non_snake_case)]
+    struct Manifest {
+        schemaVersion: u8,
+        name: String,
+        tag: String,
+        architecture: String,
+        fsLayers: Vec<BlobSummary>,
+        history: Vec<EmptyStruct>,
+        signatures: Vec<Signature>,
+
+    }
+
+    impl Manifest {
+        /* transformation function from SigningManifest -> Manifest
+         * Possibly would be worth it to think about being able to
+         * statically catch incorrect transformations when structs
+         * change shape. ie. missed values in the transformation...
+         */
+        fn from_signing_manifest(sig: &SigningManifest) -> Self {
+            Manifest {
+                schemaVersion: sig.schemaVersion.to_owned(),
+                name: sig.name.to_owned(),
+                tag: sig.tag.to_owned(),
+                architecture: sig.architecture.to_owned(),
+                fsLayers: sig.fsLayers.clone(),
+                history: sig.history.clone(),
+
+                signatures: Default::default(),
+            }
+        }
+
+    }
+
+    fn sign_manifest(digest: &str) {
+        // copying from https://github.com/ContainerSolutions/manifest-sample-python/blob/master/construct-manifest.py
+        use jwt::{Header, Token};
+        // let format_length = manifest_str.len() - 1;
+        // let format_tail = '}';
+        let header: Header = Default::default();
+        let claims = SigningManifest {
+            fsLayers: vec!(BlobSummary { blobSum: digest.to_owned() }),
+            ..Default::default()
+        };
+
+        let manifest = Manifest::from_signing_manifest(&claims);
+        let token = Token::new(header, claims);
+
+
+        println!("{:?}", manifest);
+
+        let signed = token.signed(b"secret_key", Sha256::new()).ok();
+
+        println!("{:?}", signed);
+        assert_eq!(true, false);
     }
 
     fn get_sync(url: &str) -> Result<Response, Error> {
@@ -179,30 +305,33 @@ mod interface_tests {
 
     fn get_blob() {
         //Currently have stub value in lycaon
-        let resp =
-            get_sync(&(LYCAON_ADDRESS.to_owned() + "/v2/test/test/blobs/test_digest")).unwrap();
+        let resp = get_sync(
+            &(LYCAON_ADDRESS.to_owned() + "/v2/test/test/blobs/test_digest"),
+        ).unwrap();
         assert_eq!(resp.status(), StatusCode::Ok);
         //Add test that we get file
 
         //Try getting something on another instance should redirect
 
         //Try getting something that doesn't exist
-        let resp =
-            get_sync(&(LYCAON_ADDRESS.to_owned() + "/v2/test/test/blobs/not-an-entry")).unwrap();
+        let resp = get_sync(
+            &(LYCAON_ADDRESS.to_owned() + "/v2/test/test/blobs/not-an-entry"),
+        ).unwrap();
         assert_eq!(resp.status(), StatusCode::NotFound);
     }
 
     fn unsupported() {
         //Delete currently unimplemented
-        let resp =
-            delete_sync(&(LYCAON_ADDRESS.to_owned() + "/v2/name/repo/manifests/ref")).unwrap();
+        let resp = delete_sync(&(LYCAON_ADDRESS.to_owned() + "/v2/name/repo/manifests/ref"))
+            .unwrap();
         assert_eq!(resp.status(), StatusCode::MethodNotAllowed);
     }
 
     fn upload_layer() {
         //Should support both image/test and imagetest, only former working currently
-        let resp =
-            post_sync(&(LYCAON_ADDRESS.to_owned() + "/v2/image/test/blobs/uploads/")).unwrap();
+        let resp = post_sync(
+            &(LYCAON_ADDRESS.to_owned() + "/v2/image/test/blobs/uploads/"),
+        ).unwrap();
         assert_eq!(resp.status(), StatusCode::Accepted);
         let uuid = resp.headers().get::<UploadUuid>().unwrap();
         let location = resp.headers().get::<Location>().unwrap();
@@ -218,21 +347,34 @@ mod interface_tests {
         let digest = hasher.result_str();
         let resp = put_sync(&format!(
             "{}/v2/image/test/blobs/uploads/{}?digest={}",
-            LYCAON_ADDRESS, uuid, digest
+            LYCAON_ADDRESS,
+            uuid,
+            digest
         )).unwrap();
         assert_eq!(resp.status(), StatusCode::Created);
 
         //Finally get it back again
         let resp = get_sync(&format!(
             "{}/v2/image/test/blobs/{}",
-            LYCAON_ADDRESS, digest
+            LYCAON_ADDRESS,
+            digest
         )).unwrap();
         assert_eq!(resp.status(), StatusCode::Ok);
         let mut buf = Vec::new();
         resp.body()
-            .for_each(|chunk| buf.write_all(&chunk).map(|_| ()).map_err(From::from)).wait().unwrap();
+            .for_each(|chunk| {
+                buf.write_all(&chunk).map(|_| ()).map_err(From::from)
+            })
+            .wait()
+            .unwrap();
 
         assert_eq!(blob, buf);
+    }
+
+    fn upload_manifest() {
+        // manifest with invalid layer
+        sign_manifest("invalid_digest");
+        // manifest with test/test/test_digest layer
     }
 
     #[test]
@@ -249,6 +391,8 @@ mod interface_tests {
         unsupported();
         println!("Running upload_layer()");
         upload_layer();
+        println!("Running upload_manifest()");
+        upload_manifest();
     }
 
 }
