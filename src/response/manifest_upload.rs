9cd372a32d003e454548723585758521ed75f6fd
Adrian Mouat <adrian.mouat@gmail.com>
2018-02-17 20:38:56 +0000

    Missed added file.

---
+++ src/response/manifest_upload.rs
@@ -0,0 +1,39 @@
+use rocket::http::Status;
+use rocket::response::{Responder, Response};
+use rocket::request::Request;
+use manifest::Manifest;
+use rocket::http::Header;
+
+#[derive(Debug, Serialize)]
+pub struct ManifestUpload {
+    pub location: String,
+    pub digest: String,
+}
+
+impl<'r> Responder<'r> for ManifestUpload {
+    fn respond_to(self, _: &Request) -> Result<Response<'r>, Status> {
+
+        let location = Header::new("Location", self.location);
+        let digest = Header::new("Docker-Content-Digest", self.digest);
+        Response::build()
+            .status(Status::Created)
+            .header(location)
+            .header(digest)
+            .ok()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rocket::http::Status;
+    use response::manifest_upload::ManifestUpload;
+    use manifest::Manifest;
+    use response::test_helper::test_route;
+
+    #[test]
+    fn accepted_ok() {
+        
+        let response = test_route(ManifestUpload{location: "location".to_owned(), digest: "digest".to_owned()});
+        assert_eq!(response.status(), Status::Created);
+    }
+}
