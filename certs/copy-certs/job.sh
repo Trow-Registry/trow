8c7b4b403f0c8c6aecc3cfce9426cccf7bf766ae
Adrian Mouat <adrian.mouat@gmail.com>
2018-03-28 13:00:30 +0100

    Fixes for GKE.

--- certs/copy-certs/job.sh
+++ certs/copy-certs/job.sh
@@ -11,3 +11,13 @@ echo "copying certs"
 kubectl get configmap trow-cert -n kube-public -o jsonpath='{.data.cert}'
     > "/etc/docker/certs.d/$registry_host_port/ca.crt"
 echo "Successfully copied certs"
+
+echo "Adding entry to /etc/hosts"
+# sed would be a better choice than ed, but it wants to create a temp file :(
+printf "g/$registry_host/d\nw\n" | ed /hostfile
+
+# We could use the service IP for trow, but this may not be accessible by the
+# host, so just use NodePort on our host
+echo "127.0.0.1 $registry_host # Added by trow install" >> /hostfile
+echo "Added entry"
+
