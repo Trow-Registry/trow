b6a570a72e78d4a7cfe7e828bb4cc7ba08cec458
Adrian Mouat <adrian.mouat@gmail.com>
2018-08-03 15:01:35 +0100

    Get rid of enum.

--- src/response/accepted_upload.rs
+++ src/response/accepted_upload.rs
@@ -1,27 +1,29 @@
 use failure;
-use rocket::State;
 use rocket::http::{Header, Status};
 use rocket::request::Request;
 use rocket::response::{Responder, Response};
+use rocket::State;
 
-use grpc::backend;
-use response::errors;
-use types;
 use backend as be;
+use grpc::backend;
+use types;
 
 //TODO: WTF?
 const BASE_URL: &str = "http://localhost:8000";
 
-//shouldn't be an enum
 #[derive(Debug, Serialize)]
-pub enum AcceptedUpload {
-    DigestMismatch,
-    UuidAccept {
-        uuid: String,
-        digest: String,
-        repo_name: String,
-    },
-    UuidDelete,
+pub struct AcceptedUpload {
+    uuid: String,
+    digest: String,
+    repo_name: String,
+}
+
+fn create_accepted_upload(uuid: String, digest: String, repo_name: String) -> AcceptedUpload {
+    AcceptedUpload {
+        uuid,
+        digest,
+        repo_name,
+    }
 }
 
 fn construct_digest_path(layer: &types::Layer) -> String {
@@ -63,69 +65,25 @@ impl AcceptedUpload {
         let resp = backend.delete_uuid(&layer)?;
         // 4. Construct response
         if resp.get_success() {
-            Ok(AcceptedUpload::UuidAccept {
-                uuid,
-                digest,
-                repo_name,
-            })
+            Ok(create_accepted_upload(uuid, digest, repo_name))
         } else {
             warn!("Failed to remove UUID");
             Err(failure::err_msg("Not implemented"))
-        }
-    }
-
-    pub fn delete_upload(
-        handler: State<be::BackendHandler>,
-        layer: &types::Layer,
-    ) -> Result<AcceptedUpload, errors::Error> {
-        let backend = handler.backend();
-        let mut req = backend::Layer::new();
-        req.set_repo_name(layer.repo_name.to_owned());
-        req.set_digest(layer.digest.to_owned());
-
-        //Log errors, don't send details to client
-        let response = match backend.cancel_upload(&req) {
-            Ok(r) => r,
-            Err(e) => {
-                //why can't I call error!?
-                debug!("Error calling backend {:?}", e);
-                return Err(errors::Error::InternalError);
-            }
-        };
-
-        debug!("Return: {:?}", response);
-        if response.get_success() {
-            Ok(AcceptedUpload::UuidDelete)
-        } else {
-            Err(errors::Error::BlobUploadUnknown)
         }
     }
 }
 
 impl<'r> Responder<'r> for AcceptedUpload {
     fn respond_to(self, _req: &Request) -> Result<Response<'r>, Status> {
-        use self::AcceptedUpload::{DigestMismatch, UuidAccept, UuidDelete};
 
-        match self {
-            UuidAccept {
-                repo_name, digest, ..
-            } => {
-                let location = format!("{}/v2/{}/blobs/{}", BASE_URL, repo_name, digest);
-                let location = Header::new("Location", location);
-                let digest = Header::new("Docker-Content-Digest", digest);
-                Response::build()
-                    .status(Status::Created)
-                    .header(location)
-                    .header(digest)
-                    .ok()
-            }
-            DigestMismatch => {
-                //TODO: Needs to be an error. Fix this FFS.
-                warn!("Digest mismatched");
-                Response::build().status(Status::NotFound).ok()
-            }
-            UuidDelete => Response::build().status(Status::NoContent).ok(),
-        }
+        let location = format!("{}/v2/{}/blobs/{}", BASE_URL, self.repo_name, self.digest);
+        let location_header = Header::new("Location", location);
+        let digest_header = Header::new("Docker-Content-Digest", self.digest);
+        Response::build()
+            .status(Status::Created)
+            .header(location_header)
+            .header(digest_header)
+            .ok()
     }
 }
 
@@ -139,7 +97,7 @@ mod test {
         create_upload_info(
             String::from("whatever"),
             String::from("moredhel/test"),
-            (0, 0)
+            (0, 0),
         )
     }
 
