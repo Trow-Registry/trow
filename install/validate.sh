969a4d1ed1430f1a383b4de323aba9f10ce269f1
Adrian Mouat <adrian.mouat@container-solutions.com>
2019-04-05 10:40:49 +0100

    Make validate clearer.

--- install/validate.sh
+++ install/validate.sh
@@ -5,6 +5,8 @@ IFS=$'\n\t'
 
 echo "Setting up trow as a validating webhook"
 echo "WARNING: This will limit what images can run in your cluster"
+echo "By default, only images in Trow and official Kubernetes images will be 
+allowed"
 echo
 
 cabundle=$(kubectl get configmap trow-cert -n kube-public -o jsonpath='{.data.cert}' | openssl base64 | tr -d '\n')
