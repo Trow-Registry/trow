d23707550c7f2d256eb41c9712ae7bc1f011e490
Adrian Mouat <adrian.mouat@container-solutions.com>
2019-10-22 16:07:34 +0100

    Only create cert if we don't have one.

--- install/trow-init-pod/init.sh
+++ install/trow-init-pod/init.sh
@@ -3,21 +3,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Get service IP. Not sure how essential the IP addresses are, but let's do it
-echo "Getting IP of trow service"
-SERVICE_IP=$(dig +short trow.$POD_NAMESPACE.svc.cluster.local)
-while [[ $SERVICE_IP == "" ]]
-do
-  sleep 2
+# Only do this if we don't already have a cert
+# There is a danger the cert is incorrect or expired
+# Need to add some checks
+if [ -f /certs/domain.key && -f /certs/ca.crt ]; then
+  echo "Using existing certificate"
+else
+  # Get service IP. Not sure how essential the IP addresses are, but let's do it
+  echo "Getting IP of trow service"
   SERVICE_IP=$(dig +short trow.$POD_NAMESPACE.svc.cluster.local)
-done
+  while [[ $SERVICE_IP == "" ]]
+  do
+    sleep 2
+    SERVICE_IP=$(dig +short trow.$POD_NAMESPACE.svc.cluster.local)
+  done
 
-echo "POD NAMESPACE: $POD_NAMESPACE"
-echo "POD NAME: $POD_NAME"
-echo "POD IP: $POD_IP"
-echo "SERVICE IP: $SERVICE_IP"
+  echo "POD NAMESPACE: $POD_NAMESPACE"
+  echo "POD NAME: $POD_NAME"
+  echo "POD IP: $POD_IP"
+  echo "SERVICE IP: $SERVICE_IP"
 
-cat << EOF | cfssl genkey - | cfssljson -bare trow
+  cat << EOF | cfssl genkey - | cfssljson -bare trow
 {
   "hosts": [
     "trow.$POD_NAMESPACE.svc.cluster.local",
@@ -36,18 +42,18 @@ cat << EOF | cfssl genkey - | cfssljson -bare trow
 }
 EOF
 
-# certs should be a volume that the main pod can read
-cp trow-key.pem /certs/domain.key
-echo "Saved key to /certs/domain.key"
+  # certs should be a volume that the main pod can read
+  cp trow-key.pem /certs/domain.key
+  echo "Saved key to /certs/domain.key"
 
-REQ=$(cat trow.csr | base64 | tr -d '\n')
+  REQ=$(cat trow.csr | base64 | tr -d '\n')
 
-# Change to output warning and exit instead.
-# Can't reuse CSRs due to changing IPs.
-# Can Docker clients trust k8s CA rather than certs?
-kubectl delete csr trow.$POD_NAMESPACE || true
+  # Change to output warning and exit instead.
+  # Can't reuse CSRs due to changing IPs.
+  # Can Docker clients trust k8s CA rather than certs?
+  kubectl delete csr trow.$POD_NAMESPACE || true
 
-cat <<EOF | kubectl create -f -
+  cat <<EOF | kubectl create -f -
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
 metadata:
@@ -62,7 +68,7 @@ spec:
   - server auth
 EOF
 
-echo """
+  echo """
 Waiting for CSR to be approved.
 
 Please run:
@@ -70,23 +76,30 @@ Please run:
 $ kubectl certificate approve trow.$POD_NAMESPACE
 """
 
-STAT=$(kubectl get csr trow.$POD_NAMESPACE -o json | jq -r .status.conditions[0].type)
-
-while [[ $STAT != "Approved" ]]
-do
-  sleep 10
   STAT=$(kubectl get csr trow.$POD_NAMESPACE -o json | jq -r .status.conditions[0].type)
-done
 
-kubectl get csr trow.$POD_NAMESPACE -o jsonpath='{.status.certificate}' \
-    | base64 -d > /certs/ca.crt
+  while [[ $STAT != "Approved" ]]
+  do
+    sleep 10
+    STAT=$(kubectl get csr trow.$POD_NAMESPACE -o json | jq -r .status.conditions[0].type)
+  done
 
-echo "Saved signed cert to /certs/ca.crt"
+  kubectl get csr trow.$POD_NAMESPACE -o jsonpath='{.status.certificate}' \
+      | base64 -d > /certs/ca.crt
 
-# CSRs get garbaged collected, so save the public cert to a configmap to make it
-# easy to retrieve later.
+  echo "Saved signed cert to /certs/ca.crt"
 
-# We're using the pipe through apply trick to handle case where configmap
-# already exists.
-kubectl create configmap trow-cert --from-file=cert=/certs/ca.crt \
-  --dry-run -o json | kubectl apply -n $POD_NAMESPACE -f -
+  # CSRs get garbaged collected, so save the public cert to a configmap to make it
+  # easy to retrieve later.
+
+  # We're using the pipe through apply trick to handle case where configmap
+  # already exists.
+  kubectl create configmap trow-cert --from-file=cert=/certs/ca.crt \
+    --dry-run -o json | kubectl apply -n $POD_NAMESPACE -f -
+
+  echo
+  echo "Saved cert to trow-cert config map"
+  echo
+fi
+
+echo "Init completed succesfully"
