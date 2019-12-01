a4a44692c01193a3fe5034e19c8dce8e1098969d
Shaya Potter <shaya@redislabs.com>
2019-12-01 12:11:54 +0200

    make copy-certs work with namespaces other than kube-public

--- quick-install/copy-certs/job.sh
+++ quick-install/copy-certs/job.sh
@@ -2,13 +2,13 @@
 set -e
 set -o pipefail
 
-registry_host="trow.kube-public"
+registry_host="trow.${POD_NAMESPACE}"
 registry_port="31000"
 registry_host_port="${registry_host}:${registry_port}"
 
 mkdir --parents "/etc/docker/certs.d/$registry_host_port/"
 echo "copying certs"
-kubectl get configmap trow-ca-cert -n kube-public -o jsonpath='{.data.cert}' \
+kubectl get configmap trow-ca-cert -n $POD_NAMESPACE -o jsonpath='{.data.cert}' \
     > "/etc/docker/certs.d/$registry_host_port/ca.crt"
 echo "Successfully copied certs"
 
