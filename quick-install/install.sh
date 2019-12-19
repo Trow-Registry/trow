909b6a2d9d8718791e0c34ba1f26548dc5c8d8f2
Adrian Mouat <adrian.mouat@gmail.com>
2019-12-19 16:20:42 +0000

    Remove old text from install script.

--- quick-install/install.sh
+++ quick-install/install.sh
@@ -24,9 +24,6 @@ rights:
 
   $ kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value core/account)
 
-Where <user> is your user, normally the e-mail address you use with your GKE 
-account.
-
 Also make sure port 31000 is open on the firewall so clients can connect.
 If you're running on the Google cloud, the following should work:
 
