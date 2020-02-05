950344867151e459e0e570e6e87617a6faede764
Michal Cwienczek <michael.cwienczek@gmail.com>
2020-02-05 11:13:20 +0100

    Ability to select namespace during quick install (#38)
    * WIP - ability to select namespace during quick install
    
    * Finished issue #27
    
    * Small fixes to echo messages
    
    * Double quoted namespace variables in few more places
    
    * Double quoted namespace variables in few more places
    
    * better parameter handling for configure-host.sh
    
    * Removed debugging info from scripts
    
    * Fixed typo in yaml file

--- quick-install/validate.sh
+++ quick-install/validate.sh
@@ -3,14 +3,20 @@ set -eo pipefail
 unset CDPATH
 IFS=$'\n\t'
 
+namespace='kube-public'
+if [ ! -z "$1" ]
+then
+	namespace=$1
+fi
+
 echo "Setting up trow as a validating webhook"
 echo "WARNING: This will limit what images can run in your cluster"
 echo "By default, only images in Trow and official Kubernetes images will be 
 allowed"
 echo
 
-cabundle=$(kubectl get configmap trow-ca-cert -n kube-public -o jsonpath='{.data.cert}' | openssl base64 | tr -d '\n')
+cabundle=$(kubectl get configmap trow-ca-cert -n "$namespace" -o jsonpath='{.data.cert}' | openssl base64 | tr -d '\n')
 #Really not happy about use of sed here
 #Can we use go-template now?
-sed "s/{{cabundle}}/${cabundle}/" validate-tmpl.yaml > validate.yaml
+sed "s/{{cabundle}}/${cabundle}/" validate-tmpl.yaml | sed "s/{{namespace}}/${namespace}/" > validate.yaml
 kubectl apply -f validate.yaml
