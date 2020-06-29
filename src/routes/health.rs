60677b011f10b8f38e11eb1ef3eb372b1bd336d6
Eric Caleb <caleberic@tuta.io>
2020-06-29 18:38:35 +0200

    add health route

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -3,20 +3,37 @@
 * GET /healthz
 */
 use crate::client_interface::ClientInterface;
-use crate::TrowConfig;
+// use crate::TrowConfig;
+use rocket::http::{Header, Status};
+use crate::types::HealthResponse;
 use rocket::State;
-use rocket_contrib::json::{Json, JsonValue};
+use rocket_contrib::json::{Json};
 
 use tokio::runtime::Runtime;
-
 
 #[get("/healthz")]
 pub fn healthz(
     ci: State<ClientInterface>,
-    tc: State<TrowConfig>,
-) -> JsonValue {
-    let re = ci.is_healthy();
+    // tc: State<TrowConfig>,
+) -> Json<HealthResponse> {
+    let request = ci.is_healthy();
     let mut rt = Runtime::new().unwrap();
-    rt.block_on(re).ok();
-    json!({ "status": "ok" })
+
+    match rt.block_on(request) {
+        Ok(res) => {
+            println!("responding {:?}",res);
+            Json(HealthResponse{
+                status: "OK".to_string(),
+                message: res.message,
+                is_healthy: res.is_healthy
+            })
+        },
+        Err(error) => {
+            Json(HealthResponse{
+                status: "Error".to_string(),
+                message: error.to_string(),
+                is_healthy: false
+            })
+        }
+    } 
 }
