f9a5dc5814e7791ea4b3aa298580c485293979cb
Eric Caleb <caleberic@tuta.io>
2020-07-07 16:35:51 +0200

    return HealthResponse type only

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,39 +1,35 @@
+use crate::client_interface::ClientInterface;
+use crate::types::HealthResponse;
+
+use rocket::State;
+use tokio::runtime::Runtime;
+
 /*
 * Trow health endpoint
 * GET /healthz
 */
-use crate::client_interface::ClientInterface;
-// use crate::TrowConfig;
-use rocket::http::{Header, Status};
-use crate::types::HealthResponse;
-use rocket::State;
-use rocket_contrib::json::{Json};
-
-use tokio::runtime::Runtime;
 
 #[get("/healthz")]
 pub fn healthz(
     ci: State<ClientInterface>,
-    // tc: State<TrowConfig>,
-) -> Json<HealthResponse> {
+) -> HealthResponse {
     let request = ci.is_healthy();
     let mut rt = Runtime::new().unwrap();
-
+    
     match rt.block_on(request) {
         Ok(res) => {
-            println!("responding {:?}",res);
-            Json(HealthResponse{
-                status: "OK".to_string(),
+            HealthResponse{
+                status: String::from("OK"),
                 message: res.message,
                 is_healthy: res.is_healthy
-            })
+            }
         },
         Err(error) => {
-            Json(HealthResponse{
-                status: "Error".to_string(),
+            HealthResponse{
+                status: String::from("Error"),
                 message: error.to_string(),
                 is_healthy: false
-            })
+            }
         }
     } 
 }
