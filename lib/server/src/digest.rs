c966d62df1897755c27a21c70475ee9ac7dd323b
silenteh <silenteh@gmail.com>
2020-08-13 12:21:34 +0200

    Added additional sha256 tests

--- lib/server/src/digest.rs
+++ lib/server/src/digest.rs
@@ -49,4 +49,28 @@ mod test {
         assert_eq!(result, "sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9");
     }
 
+    #[test]
+    fn sha256_digest_empty_test() {
+        let result = sha256_digest(BufReader::new("".as_bytes())).unwrap();
+        assert_eq!(result, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    }
+
+    #[test]
+    fn sha256_tag_empty_digest_test() {
+        let result = sha256_tag_digest(BufReader::new("".as_bytes())).unwrap();
+        assert_eq!(result, "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    }
+
+    #[test]
+    fn sha256_digest_brown_fox_test() {
+        let result = sha256_digest(BufReader::new("the quick brown fox jumps over the lazy dog".as_bytes())).unwrap();
+        assert_eq!(result, "05c6e08f1d9fdafa03147fcb8f82f124c76d2f70e3d989dc8aadb5e7d7450bec");
+    }
+
+    #[test]
+    fn sha256_tag_brown_fox_digest_test() {
+        let result = sha256_tag_digest(BufReader::new("the quick brown fox jumps over the lazy dog".as_bytes())).unwrap();
+        assert_eq!(result, "sha256:05c6e08f1d9fdafa03147fcb8f82f124c76d2f70e3d989dc8aadb5e7d7450bec");
+    }
+
 }
