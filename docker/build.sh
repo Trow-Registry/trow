5d36437a99977ef88a553f76660d75b0d64687d1
Ian MacGuigan <ian.macguigan@gmail.com>
2020-09-30 15:39:54 -0400

    Rework nix
    * Add .envrc for editor direnv integration
    * Rewrite default.nix and rename to shell.nix
    * Invoke bash in dev scripts via env for systems without /bin/bash (eg. NixOS)

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #change to directory with script so we know where project root is
 #https://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
--- docs/diagrams/build.sh
+++ docs/diagrams/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cat kubernetes_diagram.py | docker run -i --rm -v $PWD:/out gtramontina/diagrams:0.10.0
 cat distribution_diagram.py | docker run -i --rm -v $PWD:/out gtramontina/diagrams:0.10.0
