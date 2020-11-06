f8d7d36458668a1116455d3673bb27a01cfc3562
Adrian Mouat <adrian.mouat@gmail.com>
2020-11-06 17:01:30 +0000

    Set Docker-Content-Digest header.

--- src/response/blob_reader.rs
+++ src/response/blob_reader.rs
@@ -5,11 +5,14 @@ use rocket::response::{self, Responder, Stream};
 
 impl<'r> Responder<'r> for BlobReader {
     fn respond_to(self, req: &Request) -> response::Result<'r> {
-        let mut resp = Stream::from(self.get_reader()).respond_to(req)?;
-        //Not sure this is the right content type
+        
         let ct = Header::new("Content-Type", "application/octet-stream");
+        let digest = Header::new("Docker-Content-Digest", self.digest().0.clone());
+       
+        let mut resp = Stream::from(self.get_reader()).respond_to(req)?;
         resp.set_header(ct);
-
+        resp.set_header(digest);
+     
         Ok(resp)
     }
 }
--- tests/registry_interface.rs
+++ tests/registry_interface.rs
@@ -108,7 +108,10 @@ mod interface_tests {
             .await
             .unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
+        //Just check header exists for minute
+        resp.headers().get("Docker-Content-Digest").unwrap().to_str().unwrap();
         let mani: manifest::ManifestV2 = resp.json().await.unwrap();
+
         assert_eq!(mani.schema_version, 2);
     }
 
