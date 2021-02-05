cf2e756de675c5cbdcc4d7cff91528bf4f101f51
Eric Caleb <caleberic@tuta.io>
2021-02-05 17:51:04 +0100

    update config interface

---
+++ gui/config.ts
@@ -0,0 +1,27 @@
+const defaultRegistryURL: string = "https://trow.local:8443";
+
+interface AppConfig {
+    trow_registry_url: string;
+    app: {
+        [key: string]: app;
+    };
+}
+
+interface app {
+    authenticated: boolean;
+}
+
+// const trowRegistryURL = process.env.TROW_REGISTRY_URL
+//     ? process.env.TROW_REGISTRY_URL
+//     : defaultRegistryURL;
+
+// const config = {
+//     trow_registry_url: trowRegistryURL,
+//     app: {
+//         authenticated: false,
+//     },
+// };
+
+const config: AppConfig = require("./config.json");
+
+export default config;
