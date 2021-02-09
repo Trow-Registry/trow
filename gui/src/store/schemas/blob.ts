8208ab61922d63a98b2fac0e217bf7fa5134ff41
Eric Caleb <caleberic@tuta.io>
2021-02-09 21:27:59 +0100

    add default blob schema

--- gui/src/store/schemas/blob.ts
+++ gui/src/store/schemas/blob.ts
@@ -1,51 +1,68 @@
 export interface Blob {
     architecture: string;
-    author: string;
+    author?: string;
     os: string;
-    created: string;
-    docker_version: string;
-    config: {
-        Hostname: string;
-        Domainname: string;
-        User: string;
-        AttachStdin: boolean;
-        AttachStdout: boolean;
-        AttachStderr: boolean;
-        Tty: boolean;
-        OpenStdin: boolean;
-        StdinOnce: boolean;
-        Env: [];
-        Cmd: [];
-        Image: string;
-        Volumes: null;
-        WorkingDir: string;
-        Entrypoint: null;
-        OnBuild: null;
-        Labels: null;
-    };
+    created?: string;
+    config?: config;
     container: string;
-    container_config: {
-        Hostname: string;
-        Domainname: string;
-        User: string;
-        AttachStdin: boolean;
-        AttachStdout: boolean;
-        AttachStderr: boolean;
-        Tty: boolean;
-        OpenStdin: boolean;
-        StdinOnce: boolean;
-        Env: [];
-        Cmd: [];
-        Image: string;
-        Volumes: null;
-        WorkingDir: string;
-        Entrypoint: null;
-        OnBuild: null;
-        Labels: null;
-    };
-    history: [];
+    container_config?: config;
+    history?: blobHistory[];
+    rootfs: blobRootFS;
+}
+
+interface blobHistory {
+    created?: string;
+    author?: string;
+    created_by?: string;
+    comment?: string;
+    empty_layer?: boolean;
+}
+
+interface config {
+    Hostname?: string;
+    Domainname?: string;
+    User?: string;
+    AttachStdin?: boolean;
+    AttachStdout?: boolean;
+    AttachStderr?: boolean;
+    Tty?: boolean;
+    OpenStdin?: boolean;
+    StdinOnce?: boolean;
+    Env?: string[];
+    Cmd?: string[];
+    Image?: string;
+    Volumes?: blobConfigVolumes;
+    WorkingDir?: string;
+    Entrypoint?: string[];
+    ExposedPorts?: configExposedPorts;
+    StopSignal?: string;
+    Labels: configLabels;
+}
+
+interface configLabels {
+    [key: string]: string;
+}
+
+interface blobConfigVolumes {
+    [key: string]: {};
+}
+
+interface configExposedPorts {
+    [key: string]: {};
+}
+
+interface blobRootFS {
+    diff_ids: string[];
+    type: string;
+}
+
+
+export const defaultBlobSchema: Blob = {
+    os: "",
+    architecture: "",
     rootfs: {
-        type: string;
-        diff_ids: [];
-    };
+        type: "",
+        diff_ids: []
+    },
+    container: ""
 }
