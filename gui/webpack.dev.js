50b39f7a62798daf35fbd8d608b3fedd49b6a97a
Eric Caleb <caleberic@tuta.io>
2021-02-10 19:20:07 +0100

    use proxy option for webpack dev

--- gui/webpack.dev.js
+++ gui/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 
+const PROXY_PORT = process.env.PROXY_PORT || 9001
+
 module.exports = merge(common, {
     mode: "development",
     devServer: {
@@ -11,6 +13,13 @@ module.exports = merge(common, {
         hot: true,
         historyApiFallback: true,
         disableHostCheck: true,
+        proxy: [
+            {
+                context: ["/v2", "/login"],
+                target: `http://localhost:${PROXY_PORT}`
+            },
+        ]
     },
+
     devtool: "inline-source-map",
 });
