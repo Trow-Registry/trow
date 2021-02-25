734703b4ae44409de5c7207ecc9f749f24164704
Eric Caleb <caleberic@tuta.io>
2021-02-25 19:18:08 +0100

    add app router

---
+++ gui-wasm/src/switch.rs
@@ -0,0 +1,19 @@
+use yew_router::{components::RouterAnchor, prelude::*, switch::Permissive};
+
+#[derive(Clone, Debug, Switch)]
+pub enum AppRoute {
+    #[to = "/repositories"]
+    Repositories,
+    #[to = "/!"]
+    Home,
+    #[to = "/page-not-found"]
+    PageNotFound(Permissive<String>),
+}
+
+
+pub type AppRouter = Router<AppRoute>;  
+pub type AppAnchor = RouterAnchor<AppRoute>;
+
+// Note: using repo/tag query strings with routes did not work due to 
+// repos and tags having url unsafe strings such as "/", "-"
+// url encoding was an option but ended up using yew callbacks.
