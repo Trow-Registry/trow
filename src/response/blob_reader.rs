eaafa0ee97a6aee613317de918f164a3fcd08592
Adrian Mouat <adrian.mouat@container-solutions.com>
2021-02-27 17:21:56 +0000

    Set content-length header on blobs & manifests. (#237)
    * Set content-length header on blobs & manifests.
    
    * Add test for content size.
    
    * Run cargo fmt.

--- src/response/blob_reader.rs
+++ src/response/blob_reader.rs
@@ -1,14 +1,15 @@
 use crate::types::BlobReader;
 use rocket::http::Header;
 use rocket::request::Request;
-use rocket::response::{self, Responder, Stream};
+use rocket::response::{self, Responder, Response};
 
 impl<'r> Responder<'r> for BlobReader {
-    fn respond_to(self, req: &Request) -> response::Result<'r> {
+    fn respond_to(self, _: &Request) -> response::Result<'r> {
         let ct = Header::new("Content-Type", "application/octet-stream");
         let digest = Header::new("Docker-Content-Digest", self.digest().0.clone());
 
-        let mut resp = Stream::from(self.get_reader()).respond_to(req)?;
+        // Important to used sized_body in order to have content length set correctly
+        let mut resp = Response::build().sized_body(self.get_reader()).ok()?;
         resp.set_header(ct);
         resp.set_header(digest);
 
