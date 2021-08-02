80212b6be35ef0466180881f20b611d9046b53e5
Adrian Mouat <adrian.mouat@container-solutions.com>
2021-08-02 10:01:38 +0100

    Add timeout to ci code. (#267)

--- ci-config/install-kind-ingress.sh
+++ ci-config/install-kind-ingress.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 # Apply the ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 # Script to wait for ingress to be up and running
-# TODO: Add timeout
 ingress_running=false
-while [[ "$ingress_running" != "true" ]]
+timeout = 300 # seconds
+while [[ "$ingress_running" != "true" -a $timeout -gt 0 ]]
 do
     status=$(kubectl -n ingress-nginx get deployment ingress-nginx-controller -o jsonpath="{.status.availableReplicas}")
     if [[ "$status" = "1" ]]; then
         ingress_running=true
     fi
+    sleep 1
+    timeout--
 done
 echo "Ingress Running"
