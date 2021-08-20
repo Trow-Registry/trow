577238ba69a0a6ac654f79337030e56150f297ab
Adrian Mouat <adrian.mouat@gmail.com>
2021-08-20 14:50:42 +0100

    Rebased. Phew.

--- src/fairings/conditional_fairing.rs
+++ src/fairings/conditional_fairing.rs
@@ -4,7 +4,7 @@ pub trait AttachConditionalFairing {
     fn attach_if(self, condition: bool, fairing: impl Fairing) -> Self;
 }
 
-impl AttachConditionalFairing for Rocket {
+impl AttachConditionalFairing for Rocket<rocket::Build> {
     #[inline]
     fn attach_if(self, condition: bool, fairing: impl Fairing) -> Self {
         if condition {
