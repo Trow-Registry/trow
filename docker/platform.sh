3f6dec69c5962ca5a07881d698fb404e8a61947a
Adrian Mouat <adrian.mouat@gmail.com>
2022-01-14 12:22:27 +0000

    Single Dockerfile for all archs!
    Little bit of hackery, but surprsingly little!

---
+++ docker/platform.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case $TARGETARCH in
+
+    "amd64")
+	echo "x86_64-unknown-linux-gnu" > /.platform
+	echo "" > /.compiler 
+	;;
+    "arm64") 
+	echo "aarch64-unknown-linux-gnu" > /.platform
+	echo "gcc-aarch64-linux-gnu" > /.compiler
+	;;
+    "arm")
+	echo "armv7-unknown-linux-gnueabihf" > /.platform
+	echo "gcc-aarch64-linux-gnu" > /.compiler
+	;;
+esac
+
