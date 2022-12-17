47114980011b81936499a893c44380ad8f96e739
Adrian Mouat <adrian@chainguard.dev>
2022-12-17 22:01:54 +0000

    Try commenting out more :)

--- trow-server/src/temporary_file.rs
+++ trow-server/src/temporary_file.rs
@@ -92,7 +92,8 @@ mod test {
         assert!(!path.exists(), "File should have been deleted");
     }
 
-    //#[tokio::test]
+    /*
+    #[tokio::test]
     async fn test_temporary_file_async_cancellation() {
         let dir = tempdir().unwrap();
         let path = dir.path().join("test.txt");
@@ -119,6 +120,7 @@ mod test {
         );
         assert!(!path.exists(), "File should have been deleted");
     }
+    */
 
     #[tokio::test]
     async fn test_write() {
