248f5a293206d6fc4f5ba055726e564d06f9ab75
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-10-22 12:40:14 +0200

    rm mold

--- docker/platform.sh
+++ docker/platform.sh
@@ -5,12 +5,7 @@
 case $TARGETARCH in
 	"amd64")
 		echo "x86_64-unknown-linux-gnu" > /.platform
-		echo "clang" > /.compiler
-		mold_arch="x86_64"
-		curl -L https://github.com/rui314/mold/releases/download/v2.2.0/mold-2.2.0-x86_64-linux.tar.gz -o mold.tar.gz
-		tar -xzf mold.tar.gz
-		cp -rl mold*/* /usr
-		rm -rf mold*
+		echo "gcc" > /.compiler
 	;;
 	"arm64")
 		echo "aarch64-unknown-linux-gnu" > /.platform
