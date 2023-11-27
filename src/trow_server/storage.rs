8c99f8a2093d7bdc44fa8254b2b680d8e2abd26d
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-11-27 12:43:17 +0100

    very broken

--- src/trow_server/storage.rs
+++ src/trow_server/storage.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 use std::fs::{self, DirEntry, File};
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 use std::{io, str};
 
@@ -10,19 +11,19 @@ use crate::response::manifest_history;
 use super::errors::{DigestValidationError, ProxyError};
 use anyhow::{anyhow, Context, Result};
 use async_recursion::async_recursion;
+use bytes::Buf;
 use bytes::Bytes;
 use chrono::prelude::*;
 use futures::future::try_join_all;
 use futures::stream::StreamExt;
 use futures::Stream;
-use object_store::{path, ObjectStore, GetOptions};
+use object_store::{path, GetOptions, ObjectStore};
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::{self, Method};
 use tokio::io::AsyncWriteExt;
 use tokio::time::Duration;
 use tracing::{event, Level};
 use uuid::Uuid;
-use bytes::Buf;
 
 use super::api_types::*;
 use super::digest;
@@ -33,6 +34,22 @@ use super::server::SUPPORTED_DIGESTS;
 use super::temporary_file::TemporaryFile;
 use super::{metrics, ImageValidationConfig, RegistryProxiesConfig};
 
+// Storage Driver Error
+#[derive(thiserror::Error, Debug)]
+pub enum StorageBackendError {
+    #[error("the name `{0}` is not valid")]
+    InvalidName(String),
+    #[error("manifest is not valid")]
+    InvalidManifest,
+    #[error("Digest did not match content")]
+    InvalidDigest,
+    #[error("Unsupported Operation")]
+    Unsupported,
+    #[error("Requested index does not match actual")]
+    InvalidContentRange,
+    #[error("Internal storage error")]
+    Internal,
+}
 
 #[derive(Clone, Debug)]
 pub struct TrowStorageBackend {
@@ -44,6 +61,14 @@ pub struct TrowStorageBackend {
 }
 
 impl TrowStorageBackend {
+    pub fn new(url: String) -> Result<Self> {
+        let url = reqwest::Url::from_str(&url).unwrap();
+        let (store, path ) = object_store::parse_url(&url).unwrap();
+        Ok(Self {
+            object_store: Arc::from(store),
+        })
+    }
+
     pub fn get_object_store(&self) -> &dyn ObjectStore {
         &*self.object_store
     }
@@ -65,6 +90,8 @@ impl TrowStorageBackend {
 
         Ok(latest_digest.to_string())
     }
+
+    async fn get_blob ()
 
     pub async fn write_blob_stream<S>(&self, digest: &str, mut stream: S) -> Result<()>
     where
@@ -91,7 +118,12 @@ impl TrowStorageBackend {
         Ok(())
     }
 
-    pub async fn write_image_manifest(&self, manifest: Bytes, repo_name: &str, tag: &str) -> Result<()> {
+    pub async fn write_image_manifest(
+        &self,
+        manifest: Bytes,
+        repo_name: &str,
+        tag: &str,
+    ) -> Result<()> {
         let digest = digest::sha256_digest(manifest.as_ref().reader())?;
 
         // write image manifest as a blob
@@ -100,7 +132,7 @@ impl TrowStorageBackend {
             Ok(_) => {
                 event!(Level::INFO, "Manifest already exists");
             }
-            Err(object_store::Error::NotFound{..}) => {
+            Err(object_store::Error::NotFound { .. }) => {
                 event!(Level::INFO, "Writing manifest");
                 self.object_store.put(&manifest_blob_loc, manifest).await?;
             }
@@ -116,10 +148,12 @@ impl TrowStorageBackend {
             match get_object {
                 Ok(get_result) => {
                     let bytes = get_result.bytes().await?;
-                    let history = serde_json::from_slice(&bytes).context("Could not parse manifest history")?;
-                    history
-                },
-                Err(object_store::Error::NotFound{..}) => Vec::new(),
+                    serde_json::from_slice(&bytes).unwrap_or_else(|_| {
+                        event!(Level::WARN, "Could not parse manifest history");
+                        Vec::new()
+                    })
+                }
+                Err(object_store::Error::NotFound { .. }) => Vec::new(),
                 Err(e) => return Err(anyhow!(e).context("Could not GET manifest history file")),
             }
         };
@@ -128,40 +162,83 @@ impl TrowStorageBackend {
             timestamp: Utc::now().to_rfc3339_opts(SecondsFormat::Nanos, true),
         });
         let manifest_history_bytes = Bytes::from(serde_json::to_vec(&manifest_history)?);
-        self.object_store.put(&manifest_history_loc, manifest_history_bytes).await?;
+        self.object_store
+            .put(&manifest_history_loc, manifest_history_bytes)
+            .await?;
 
         Ok(())
     }
 
-    pub async fn get_manifest_history(&self, repo_name: &str, tag: &str) -> Result<Vec<ManifestHistoryEntry>> {
+    pub async fn get_manifest_history(
+        &self,
+        repo_name: &str,
+        tag: &str,
+    ) -> Result<Vec<ManifestHistoryEntry>> {
         let manifest_history_loc = path::Path::from_iter(["manifests", repo_name, tag]);
-        let get_result = self.object_store.get(&manifest_history_loc).await.context("Could not GET manifest history file")?;
+        let get_result = self
+            .object_store
+            .get(&manifest_history_loc)
+            .await
+            .context("Could not GET manifest history file")?;
         let bytes = get_result.bytes().await?;
         let history = serde_json::from_slice(&bytes).context("Could not parse manifest history")?;
         Ok(history)
     }
 
+    pub async fn list_repos(&self) -> Result<Vec<String>> {
+        let manifest_dir = path::Path::from_iter(["manifests"]);
+        let repo_paths = self
+            .object_store
+            .list_with_delimiter(Some(&manifest_dir))
+            .await?
+            .common_prefixes;
 
+        let entries = repo_paths
+            .into_iter()
+            .map(|p| {
+                let p = p.to_string();
+                let (_, repo) = p.split_once('/').unwrap();
+                repo.to_owned()
+            })
+            .collect();
+
+        Ok(entries)
+    }
+
+    pub async fn list_repo_tags(&self, repo: &str) -> Result<Vec<String>> {
+        let mut manifests = Vec::new();
+        let repo_dir = path::Path::from_iter(["manifests", repo]);
+        let mut manifest_iter = self.object_store.list(Some(&repo_dir));
+        while let Some(manifest) = manifest_iter.next().await {
+            let manifest = manifest.context("Could not list manifest")?;
+            let path = String::from(manifest.location);
+            let invalid_manifest_path_err = || anyhow!("Invalid manifest path `{path}`");
+            let (_, tag) = path
+                .rsplit_once("/")
+                .ok_or_else(invalid_manifest_path_err)?;
+            manifests.push(tag.to_owned());
+        }
+        Ok(manifests)
+    }
 }
 
-pub fn get_catalog_path_for_blob(digest: &str) -> Result<PathBuf> {
-    let mut iter = digest.split(':');
-    let alg = iter
-        .next()
-        .ok_or_else(|| anyhow!("Digest '{digest}' did not contain alg component"))?;
-    if !SUPPORTED_DIGESTS.contains(&alg) {
-        return Err(anyhow!("Hash algorithm '{alg}' not supported"));
-    }
-    let val = iter
-        .next()
-        .ok_or_else(|| anyhow!("Digest '{digest}' did not contain value component"))?;
-    if let Some(val) = iter.next() {
-        return Err(anyhow!("Digest '{digest}' contains too many elements"));
-    }
+// pub fn get_catalog_path_for_blob(digest: &str) -> Result<PathBuf> {
+//     let mut iter = digest.split(':');
+//     let alg = iter
+//         .next()
+//         .ok_or_else(|| anyhow!("Digest '{digest}' did not contain alg component"))?;
+//     if !SUPPORTED_DIGESTS.contains(&alg) {
+//         return Err(anyhow!("Hash algorithm '{alg}' not supported"));
+//     }
+//     let val = iter
+//         .next()
+//         .ok_or_else(|| anyhow!("Digest '{digest}' did not contain value component"))?;
+//     if let Some(val) = iter.next() {
+//         return Err(anyhow!("Digest '{digest}' contains too many elements"));
+//     }
 
-    Ok(PathBuf::from("blobs").join(alg).join(val))
-}
-
+//     Ok(PathBuf::from("blobs").join(alg).join(val))
+// }
 
 fn does_manifest_match_digest(manifest: &DirEntry, digest: &str) -> bool {
     digest
@@ -174,68 +251,33 @@ fn does_manifest_match_digest(manifest: &DirEntry, digest: &str) -> bool {
         }
 }
 
-struct RepoIterator {
-    paths: Vec<Result<DirEntry, std::io::Error>>,
-}
+// /**
+//  * Checks a file matches the given digest.
+//  *
+//  * TODO: should be able to use range of hashes.
+//  * TODO: check if using a static for the hasher speeds things up.
+//  */
+// fn validate_digest(file: &PathBuf, digest: &str) -> Result<()> {
+//     let f = File::open(file)?;
+//     let reader = BufReader::new(f);
 
-impl RepoIterator {
-    fn new(base_dir: &Path) -> Result<RepoIterator> {
-        let paths = fs::read_dir(base_dir)?.collect();
-        Ok(RepoIterator { paths })
-    }
-}
+//     let calculated_digest = sha256_tag_digest(reader)?;
 
-impl Iterator for RepoIterator {
-    type Item = DirEntry;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.paths.pop() {
-            None => None,
-            Some(res_path) => match res_path {
-                Err(e) => {
-                    event!(Level::WARN, "Error iterating over repos {:?}", e);
-                    self.next()
-                }
-                Ok(path) => {
-                    if path.file_type().unwrap().is_dir() {
-                        let new_paths = fs::read_dir(path.path()).unwrap();
-                        self.paths.extend(new_paths);
-                        self.next()
-                    } else {
-                        Some(path)
-                    }
-                }
-            },
-        }
-    }
-}
+//     if calculated_digest != digest {
+//         event!(
+//             Level::ERROR,
+//             "Upload did not match given digest. Was given {} but got {}",
+//             digest,
+//             calculated_digest
+//         );
+//         return Err(anyhow!(DigestValidationError {
+//             user_digest: digest.to_string(),
+//             actual_digest: calculated_digest,
+//         }));
+//     }
 
-/**
- * Checks a file matches the given digest.
- *
- * TODO: should be able to use range of hashes.
- * TODO: check if using a static for the hasher speeds things up.
- */
-fn validate_digest(file: &PathBuf, digest: &str) -> Result<()> {
-    let f = File::open(file)?;
-    let reader = BufReader::new(f);
-
-    let calculated_digest = sha256_tag_digest(reader)?;
-
-    if calculated_digest != digest {
-        event!(
-            Level::ERROR,
-            "Upload did not match given digest. Was given {} but got {}",
-            digest,
-            calculated_digest
-        );
-        return Err(anyhow!(DigestValidationError {
-            user_digest: digest.to_string(),
-            actual_digest: calculated_digest,
-        }));
-    }
-
-    Ok(())
-}
+//     Ok(())
+// }
 
 fn is_path_writable(path: &PathBuf) -> io::Result<bool> {
     let file = File::open(path)?;
@@ -257,4 +299,20 @@ fn get_digest_from_manifest_path<P: AsRef<Path>>(path: P) -> Result<String> {
         .ok_or_else(|| anyhow!("Invalid manifest line: `{}`", latest_digest_line))?;
 
     Ok(latest_digest.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use tempfile::tempdir;
+
+    #[test]
+    fn build_object_store() {
+        let dir = tempdir().unwrap();
+        let url = format!("file://{}", dir.path().to_str().unwrap());
+        let store = TrowStorageBackend::new(url.to_string()).unwrap();
+    }
+
+
 }
