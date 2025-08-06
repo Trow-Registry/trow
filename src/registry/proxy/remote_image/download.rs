cccb03b768222ae20d68f3d92a40f9b2dd075e7e
Arthur Woimbée <arthur.woimbee@gmail.com>
2025-08-06 17:12:12 +0200

    auto proxy ECR (no need for AWS username)

--- src/registry/proxy/remote_image/download.rs
+++ src/registry/proxy/remote_image/download.rs
@@ -4,6 +4,8 @@ use futures::future::try_join_all;
 use oci_client::Reference;
 use oci_client::client::ClientProtocol;
 use oci_client::secrets::RegistryAuth;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 use crate::TrowServerState;
 use crate::registry::digest::Digest;
@@ -114,23 +116,31 @@ async fn get_oci_client(
     host: &str,
     cfg: Option<&SingleRegistryProxyConfig>,
 ) -> Result<(oci_client::Client, RegistryAuth), DownloadRemoteImageError> {
+    lazy_static! {
+        static ref REGEX_PRIVATE_ECR: Regex = Regex::new(r"^[0-9]+\.dkr\.ecr\.[a-z0-9-]+\.amazonaws.com$").unwrap();
+    }
+
     let mut client_config = oci_client::client::ClientConfig::default();
     if cfg.is_some_and(|c| c.insecure) {
         client_config.protocol = ClientProtocol::Http;
     }
     let client = oci_client::Client::new(client_config);
     let auth = match cfg.and_then(|c| c.username.as_deref()) {
-        Some(u @ "AWS") if host.contains(".dkr.ecr.") => {
-            let passwd = get_aws_ecr_password_from_env(host).await?;
-            RegistryAuth::Basic(u.to_string(), passwd)
-        }
         Some(u) => RegistryAuth::Basic(
             u.to_string(),
             cfg.map(|c| c.password.clone().unwrap_or_default())
                 .unwrap_or_default(),
         ),
-        None => RegistryAuth::Anonymous,
+        None => {
+            if REGEX_PRIVATE_ECR.is_match(host) {
+                let passwd = get_aws_ecr_password_from_env(host).await?;
+                RegistryAuth::Basic("AWS".to_string(), passwd)
+            } else {
+                RegistryAuth::Anonymous
+            }
+        }
     };
+
     // client.auth(&image.clone().into(), &auth, RegistryOperation::Pull).await?;
     Ok((client, auth))
 }
@@ -254,4 +264,29 @@ async fn get_aws_ecr_password_from_env(ecr_host: &str) -> Result<String, EcrPass
     auth_str.drain(0..4);
 
     Ok(String::from_utf8(auth_str)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_oci_client_no_cfg() {
+        let (_clt, auth) = get_oci_client("example.com", None).await.unwrap();
+        assert!(matches!(auth, RegistryAuth::Anonymous));
+    }
+    #[tokio::test]
+    async fn test_get_oci_client_no_cfg_ecr() {
+        let err = get_oci_client("1234.dkr.ecr.mars-1.amazonaws.com", None).await;
+        assert!(matches!(err, Err(DownloadRemoteImageError::EcrLoginError(_))));
+    }
+    #[tokio::test]
+    async fn test_get_oci_client_cfg_basic() {
+        let proxy_cfg = SingleRegistryProxyConfig {
+            username: Some("Jacky".to_string()),
+            ..Default::default()
+        };
+        let (_clt, auth) = get_oci_client("prout.oups", Some(&proxy_cfg)).await.unwrap();
+        assert_eq!(auth, RegistryAuth::Basic("Jacky".to_string(), String::new()));
+    }
 }
