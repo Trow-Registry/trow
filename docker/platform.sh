6b9050bab67fbff456301ce691faabaeb43ab2bd
Arthur Woimbée <arthur.woimbee@gmail.com>
2025-08-06 10:56:00 +0200

    small updates to container build process

--- docker/platform.sh
+++ docker/platform.sh
@@ -2,16 +2,19 @@
 
 # Used in Docker build to set platform dependent variables
 
+function install_mold() {
+    curl -L https://github.com/rui314/mold/releases/download/v2.40.3/mold-2.40.3-$1-linux.tar.gz -o mold.tar.gz
+	tar -xzf mold.tar.gz
+	cp -rl mold*/* /usr
+	rm -rf mold*
+}
+
 case $TARGETARCH in
 	"amd64")
 		echo "Building for amd64"
 		echo "x86_64-unknown-linux-gnu" > /.platform
 		echo "clang" > /.compiler
-		mold_arch="x86_64"
-		curl -L https://github.com/rui314/mold/releases/download/v2.2.0/mold-2.2.0-x86_64-linux.tar.gz -o mold.tar.gz
-		tar -xzf mold.tar.gz
-		cp -rl mold*/* /usr
-		rm -rf mold*
+		install_mold "x86_64"
 	;;
 	"arm64")
 		echo "Building for arm64"
